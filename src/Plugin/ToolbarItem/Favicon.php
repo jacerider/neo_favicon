ad3b6a88ff0ec968f821a32b20e6ce9732ae3547
JaceRider <jacerider@gmail.com>
2025-10-08 11:14:10 -0500

    ✨ (Favicon.php): add image filter options to the Favicon toolbar item form to enhance customization 🐛 (Favicon.php): fix image rendering to apply selected CSS filter dynamically 📝 (Favicon.php): update documentation for ajaxPreview and getFilterOptions methods to improve clarity

--- src/Plugin/ToolbarItem/Favicon.php
+++ src/Plugin/ToolbarItem/Favicon.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\neo_favicon\Plugin\ToolbarItem;
 
 use Drupal\Component\Transliteration\TransliterationInterface;
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Render\RendererInterface;
@@ -16,6 +18,7 @@ use Drupal\neo_toolbar\ToolbarItemColorSchemeTrait;
 use Drupal\neo_toolbar\ToolbarItemElement;
 use Drupal\neo_toolbar\ToolbarItemPluginBase;
 use Drupal\neo_toolbar\ToolbarItemLinkTrait;
+use Drupal\neo_tooltip\Tooltip;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -83,6 +86,7 @@ final class Favicon extends ToolbarItemPluginBase {
       'target' => '',
       'image' => '',
       'scheme' => '',
+      'filter' => '',
     ];
   }
 
@@ -91,6 +95,7 @@ final class Favicon extends ToolbarItemPluginBase {
    */
   public function itemForm(array $form, FormStateInterface $form_state, array &$complete_form): array {
     $form = parent::itemForm($form, $form_state, $complete_form);
+    $id = Html::getId('toolbar-item-' . $this->pluginId);
 
     $form['url'] = $this->urlForm([], $form_state, $this->configuration['url']);
 
@@ -106,14 +111,16 @@ final class Favicon extends ToolbarItemPluginBase {
       if ($data['width'] > 100) {
         $neoImageStyle = new NeoImageStyle();
         $neoImageStyle->cropSides();
-        $neoImageStyle->scale(36, 36);
+        $neoImageStyle->exact(36, 36);
         $build = $neoImageStyle->toRenderableFromUri($uri);
         $build['#prefix'] = '<div class="flex items-center justify-center bg-primary-500 w-12 h-12 p-1 rounded">';
         $build['#suffix'] = '</div>';
         $build['#attributes']['class'][] = 'w-auto';
-        if (strpos($uri, 'mstile')) {
-          $build['#attributes']['style'] = 'filter: brightness(0) invert(1);';
+        if ($filter = $this->configuration['filter']) {
+          $build['#attributes']['style'] = $filter;
         }
+        $tooltip = new Tooltip($uri);
+        $tooltip->applyTo($build);
         $build = $this->renderer->render($build);
         $options[$uri] = Markup::create($build);
       }
@@ -121,16 +128,38 @@ final class Favicon extends ToolbarItemPluginBase {
 
     $form['image'] = [
       '#type' => 'radios',
-      '#title' => $this->t('Image'),
+      '#title' => $this->t('Image') . time(),
       '#options' => $options,
       '#required' => TRUE,
       '#description' => $this->t('The favicon image.'),
       '#default_value' => $this->configuration['image'],
+      '#prefix' => '<div id="' . $id . '-image">',
+      '#suffix' => '</div>',
+    ];
+
+    $form['filter'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Image Filter'),
+      '#options' => $this->getFilterOptions(),
+      '#default_value' => $this->configuration['filter'],
+      '#description' => $this->t('Apply a CSS filter to the image.'),
+      '#ajax' => [
+        'callback' => [self::class, 'ajaxPreview'],
+        'wrapper' => $id . '-image',
+      ],
     ];
 
     $form['scheme'] = $this->getSchemeElement($this->configuration['scheme']);
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function ajaxPreview(array $form, FormStateInterface $form_state): array {
+    $parents = array_splice($form_state->getTriggeringElement()['#array_parents'], 0, -1);
+    return NestedArray::getValue($form, $parents)['image'];
   }
 
   /**
@@ -153,12 +182,26 @@ final class Favicon extends ToolbarItemPluginBase {
       $path = \Drupal::service('extension.list.module')->getPath('neo_favicon');
       $element->setImage('/' . $path . '/images/favicon.png');
     }
-    if (strpos($this->configuration['image'], 'mstile')) {
-      $element->setImageAttribute('style', 'filter: brightness(0) invert(1);');
+    if ($filter = $this->configuration['filter']) {
+      $element->setImageAttribute('style', $filter);
     }
     $this->processSchemeElement($element);
     $this->linkProcessElement($element);
     return $element;
+  }
+
+  /**
+   * Get filter options.
+   *
+   * @return array
+   *   The filter options.
+   */
+  protected function getFilterOptions(): array {
+    return [
+      '' => $this->t('None'),
+      'filter: brightness(0) invert(1);' => $this->t('Style 1'),
+      'mix-blend-mode: screen; filter: invert(1) brightness(2);' => $this->t('Style 2'),
+    ];
   }
 
 }
